47328b06d5341178c00a271e95271e552db29cb6
amazing955 <amazingclinton256@gmail.com>
2025-03-24 11:53:13 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,56 +6,46 @@
     int chemistry[4];
     int average[4];
     char tests[4][20] = {"coursework", "assignment", "mid-term", "end-of-term"};
-
-    // Ask for Math Test Scores
-    printf("Enter Maths Scores\n");
+    printf("Enter Maths marks\n");
     for (int i = 0; i < 4; i++)
     {
         printf("  Enter Score for %s; ", tests[i]);
         // statement above will ensure we only ask 4 times
         scanf("%d", &mathsArray[i]);
     }
-
-    printf("=======\n");
-    printf("----> Maths Average score is %d\n", registerMarks(mathsArray));
-    printf("=======\n");
-
-    // Ask for physics Test Scores
-    printf("Enter Physics Scores\n");
+    printf("\n");
+    printf("Maths Average marks is %d\n", registerMarks(mathsArray));
+    printf("Enter Physics marks\n");
     for (int i = 0; i < 4; i++)
     {
-        printf("  Enter Score for %s; ", tests[i]);
-        // statement above will ensure we only ask 4 times
+        printf("  Enter marks for %s; ", tests[i]);
         scanf("%d", &physics[i]);
     }
 
-    printf("=======\n");
-    printf("----> Physics Average score is %d \n", registerMarks(physics));
-    printf("=======\n");
+    printf("\n");
+    printf("Physics Average marks is %d \n", registerMarks(physics));
 
-    // Ask for chemistry Test Scores
-    printf("Enter Chemistry Scores\n");
+    printf("Enter Chemistry marks\n");
     for (int i = 0; i < 4; i++)
     {
-        printf("  Enter Score for %s; ", tests[i]);
-        // statement above will ensure we only ask 4 times
+        printf("Enter marks for %s; ", tests[i]);
+        
         scanf("%d", &chemistry[i]);
     }
 
-    printf("=======\n");
-    printf("----> Chemistry Average score is %d \n", registerMarks(chemistry));
-    printf("=======\n");
+    printf("\n");
+    printf("Chemistry Average mark is %d \n", registerMarks(chemistry));
+    
 
     return 0;
  }
 
- int registerMarks(int *someArray){
-    // this function takes in a parameter callend someArray
-    int arrLength = sizeof(someArray) / 2;
+ int registerMarks(int *myarray){
+    int arrLength = sizeof(myarray) / 2;
     int sum = 0;
 
     for(int i=0; i<arrLength; i++){
-        sum += someArray[i];
+        sum += myarray[i];
     }
 
     int averageScore = sum / arrLength;
